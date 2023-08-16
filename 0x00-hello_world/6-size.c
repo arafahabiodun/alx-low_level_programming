848d710110ff65e2b2eb25a9f96fbb1388e42e14
arafahabiodun <arafahabiodun@gmail.com>
2023-08-16 04:23:29 -0700

    6size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %i byte(S)\n", sizeof(char));
-	printf("Size of a int: %i byte(S)\n", sizeof(int));
-	printf("Size of a long int: %i byte(S)\n", sizeof(long int));
-	printf("Size of a long long int: %i byte(S)\n", sizeof(long long int));
+	printf("Size of a char: %d byte(S)\n", sizeof(char));
+	printf("Size of a int: %d byte(S)\n", sizeof(int));
+	printf("Size of a long int: %d byte(S)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(S)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(S)\n", sizeof(float));
 	return (0);
 }
