40ca75940dedc4979d1b0f1a5f41652e6ca2ab31
arafahabiodun <arafahabiodun@gmail.com>
2023-08-22 09:36:08 -0700

    Reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  by Arafah
+*  Return: 0 is success
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
